5e2a4ef255a7e7153a5234d51f43d9a13c821284
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-07-30 09:15:02 -0600

    Initial commit
    main.py and unit test file set

---
+++ main-test.py
@@ -0,0 +1,11 @@
+# Importing the Add function from main.
+from main import Add
+
+
+def TestAdd():
+    assert Add(2, 3) == 5
+    print("Add Function works correctly")
+
+
+if __name__ == '__main__':
+    TestAdd()
